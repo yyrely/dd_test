69e5907a6daf644df8e91db4029e070f588e2d19
yyrely <734953453@qq.com>
2020-04-26 14:58:00 +0800

    fix

---
+++ src/main/java/com/chuncongcong/test/config/RestTemplateConfig.java
@@ -0,0 +1,37 @@
+package com.chuncongcong.test.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.client.ClientHttpRequestFactory;
+import org.springframework.http.client.SimpleClientHttpRequestFactory;
+import org.springframework.web.client.RestTemplate;
+
+/**
+ * @author HU
+ * @date 2020/4/26 14:57
+ */
+
+
+@Configuration
+public class RestTemplateConfig {
+
+
+	//最好是用不注释的方法，在注入的同时设置连接时间，这种注释的也可以，但是没有设置超时时间
+    /*@Bean
+    public RestTemplate restTemplate(RestTemplateBuilder builder){
+        return builder.build();
+    }*/
+
+	@Bean
+	public RestTemplate restTemplate(ClientHttpRequestFactory factory){
+		return new RestTemplate(factory);
+	}
+
+	@Bean
+	public ClientHttpRequestFactory simpleClientHttpRequestFactory(){
+		SimpleClientHttpRequestFactory factory = new SimpleClientHttpRequestFactory();
+		factory.setReadTimeout(5000);//单位为ms
+		factory.setConnectTimeout(5000);//单位为ms
+		return factory;
+	}
+}
