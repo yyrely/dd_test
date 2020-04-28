aa13a26a82bc659f3aea6796d83792505a273dbd
yyrely <734953453@qq.com>
2020-04-28 20:47:59 +0800

    fix

--- src/main/java/com/chuncongcong/test/controller/CallBackRegisterController.java
+++ src/main/java/com/chuncongcong/test/controller/CallBackRegisterController.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
@@ -66,7 +67,7 @@ public class CallBackRegisterController {
 		log.info("getCallBackResponse: {}", getCallBackResponse);
 	}
 
-	@GetMapping("/callBack/register/callback")
+	@PostMapping("/callBack/register/callback")
 	public Map<String, String> callBack(@RequestBody JsonNode jsonNode, String signature, String timestamp, String nonce) {
 		String decryptText = decryptText(signature, timestamp, nonce, jsonNode.get("encrypt").textValue());
 		JsonNode decryJsonNode = JacksonUtils.jsonToTree(decryptText);
