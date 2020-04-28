2e372dfbade2895b7bfc7a730f5d9b59b254d8a9
yyrely <734953453@qq.com>
2020-04-28 20:46:06 +0800

    fix

--- src/main/java/com/chuncongcong/test/controller/CallBackRegisterController.java
+++ src/main/java/com/chuncongcong/test/controller/CallBackRegisterController.java
@@ -1,17 +1,22 @@
 package com.chuncongcong.test.controller;
 
 import java.util.HashMap;
+import java.util.LinkedHashMap;
 import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
 import com.chuncongcong.test.config.DingProperties;
+import com.chuncongcong.test.utils.DingTalkEncryptException;
+import com.chuncongcong.test.utils.DingTalkEncryptor;
 import com.chuncongcong.test.utils.JacksonUtils;
+import com.chuncongcong.test.utils.Utils;
 import com.fasterxml.jackson.databind.JsonNode;
 
 import lombok.extern.slf4j.Slf4j;
@@ -47,7 +52,7 @@ public class CallBackRegisterController {
 		registerCallBody.put("call_back_tag", JacksonUtils.toJson(new String[] {"bpms_instance_change","bpms_task_change"}));
 		registerCallBody.put("token", dingProperties.getSuiteToken());
 		registerCallBody.put("aes_key", dingProperties.getEncodingAESKey());
-		registerCallBody.put("url", "http://www.chuncongcong.com:8030/callback");
+		registerCallBody.put("url", "http://www.chuncongcong.com:8030/callBack/register/callback");
 		HttpEntity<Map<String, String>> registerCallRequest = new HttpEntity<>(registerCallBody);
 		ResponseEntity<String> registerCallEntity = restTemplate.postForEntity(
 				"https://oapi.dingtalk.com/call_back/register_call_back?access_token={accessToken}",
@@ -60,4 +65,61 @@ public class CallBackRegisterController {
 		JsonNode getCallBackResponse = JacksonUtils.jsonToTree(getCallBackEntity.getBody());
 		log.info("getCallBackResponse: {}", getCallBackResponse);
 	}
+
+	@GetMapping("/callBack/register/callback")
+	public Map<String, String> callBack(@RequestBody JsonNode jsonNode, String signature, String timestamp, String nonce) {
+		String decryptText = decryptText(signature, timestamp, nonce, jsonNode.get("encrypt").textValue());
+		JsonNode decryJsonNode = JacksonUtils.jsonToTree(decryptText);
+
+		Map<String, String> resultMap = new HashMap<>();
+
+		switch (decryJsonNode.get("EventType").textValue()) {
+			case "bpms_instance_change":
+				log.info("[callback] 审批事件回调:{}", decryJsonNode);
+				String processInstanceId = decryJsonNode.get("processInstanceId").textValue();
+				String type = decryJsonNode.get("type").textValue();
+				log.info("processInstanceId: {}", processInstanceId);
+				log.info("type: {}", type);
+				break;
+			case "check_url":
+				log.info("[callback] 验证注册回调地址有效性质:{}", decryJsonNode);
+				resultMap = encryptText("success");
+				break;
+			default:
+				break;
+		}
+
+		return resultMap;
+	}
+
+	private Map<String, String> encryptText(String text) {
+		Map<String, String> resultMap = new LinkedHashMap<>();
+		try {
+			DingTalkEncryptor dingTalkEncryptor = new DingTalkEncryptor(dingProperties.getSuiteToken(),
+					dingProperties.getEncodingAESKey(), "dingb313afaa42af4ed5");
+			resultMap = dingTalkEncryptor.getEncryptedMap(text, System.currentTimeMillis(), Utils.getRandomStr(8));
+		} catch (DingTalkEncryptException e) {
+			log.error("钉钉消息体加密,text: {}, e: {}", text, e);
+		}
+		log.debug("钉钉消息体加密,text: {}, resultMap: {}", text, resultMap);
+		return resultMap;
+	}
+
+
+
+	private String decryptText(String signature, String timestamp, String nonce, String encryptMsg) {
+		String plainText = "";
+		try {
+			DingTalkEncryptor dingTalkEncryptor = new DingTalkEncryptor(dingProperties.getSuiteToken(),
+					dingProperties.getEncodingAESKey(), "dingb313afaa42af4ed5");
+			plainText = dingTalkEncryptor.getDecryptMsg(signature, timestamp, nonce, encryptMsg);
+		} catch (DingTalkEncryptException e) {
+			log.error("钉钉消息体解密错误, signature: {}, timestamp: {}, nonce: {}, encryptMsg: {}, e: {}", signature, timestamp,
+					nonce, encryptMsg, e);
+		}
+		log.debug("钉钉消息体解密, signature: {}, timestamp: {}, nonce: {}, encryptMsg: {}, 解密结果: {}", signature, timestamp,
+				nonce, encryptMsg, plainText);
+		return plainText;
+	}
+
 }
